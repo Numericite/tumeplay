f69c10052c13341d39d54f59f263dc2928d48310
BrianRid <brianridolce@gmail.com>
2024-10-10 16:15:37 +0200

    feat: updating package.json

--- tumeplay-app/src/hooks/autoScrollToTop.js
+++ tumeplay-app/src/hooks/autoScrollToTop.js
@@ -2,13 +2,19 @@ import {Platform} from 'react-native';
 
 const autoScrollToTop = props => {
   if (Platform.OS === 'web') {
-    const didFocusSubscription = props.navigation.addListener('focus', () => {
-      window.scrollTo(0, 0);
-    });
-    const willBlurSubscription = props.navigation.addListener('blur', () => {
-      didFocusSubscription.remove();
-      willBlurSubscription.remove();
-    });
+    const didFocusSubscription = props.navigation.addListener(
+      'didFocus',
+      () => {
+        window.scrollTo(0, 0);
+      },
+    );
+    const willBlurSubscription = props.navigation.addListener(
+      'willBlur',
+      () => {
+        didFocusSubscription.remove();
+        willBlurSubscription.remove();
+      },
+    );
   }
 };
 
